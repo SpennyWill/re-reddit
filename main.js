41071909be4e4ee348d25ad1a1a8bc5a72c28616
Spencer Willis <spencerwillis@spencers-air.attlocal.net>
2024-06-06 20:44:03 -0400

    made posts and comments

--- main.js
+++ main.js
@@ -1,23 +1,56 @@
 document.getElementById('submit').addEventListener('click', (event) => {
-  event.preventDefault(); 
+  event.preventDefault();
+
+  let postAuthor = document.getElementsByClassName('p-name')[0].value;
+  let postContent = document.getElementsByClassName('post')[0].value;
+
+  let post = `"${postContent}" Posted by ${postAuthor}`;
+  let entry = document.createElement('p');
+  entry.setAttribute("id", randomId());
+  entry.innerHTML = post;
+
+  
+  let removeButton = document.createElement('button');
+  removeButton.textContent = "Remove";
   
 
-let postAuthor = document.getElementsByClassName('name')[0].value;
+  
+  let createPost = document.getElementsByClassName('post-section')[0];
+  createPost.appendChild(entry);
+  entry.appendChild(removeButton);
 
-let postComment = document.getElementsByClassName('comment')[0].value; 
+  document.getElementsByClassName('name')[0].value = '';
+  document.getElementsByClassName('comment')[0].value = '';
+  removeButton.addEventListener("click", function() {
+    removePost(this.parentNode.id)
 
-let post =  `"${postComment}" Posted by ${postAuthor}  `; 
+  });
+});
 
 
 
-let entry = document.createElement('p')
 
-let comment = document.getElementsByClassName('form-inline')[0].appendChild(entry)
-  entry.innerHTML = post; 
+document.getElementById('comment').addEventListener('click', (event) => {
+  event.preventDefault(); 
+  let commentContent = document.getElementsByClassName('comment')[0].value = ''
+  let commentAuthor = document.getElementsByClassName('c-name')[0].value;
+  let comment = `${commentAuthor} said "${commentContent}"`;
 
-  document.getElementsByClassName('name')[0].value = ''
-  document.getElementsByClassName('comment')[0].value ='' 
+  let nextComment = document.createElement('p')
+
+  let createComment = document.getElementsByClassName('post-section')[0].appendChild(nextComment)
+  nextComment.innerHTML = comment; 
+
 }); 
 
+function randomId () {
+ var id = Math.floor(Math.random()*100000000) 
+ return id
+};
+
+function removePost (id) {
+  document.getElementById(id).remove(); 
+
+}
 
 //Test for github
