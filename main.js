ad4fa90ab4ef5aea8d40a95941977812429a2c40
Spencer Willis <spencerwillis@spencers-air.attlocal.net>
2024-06-11 20:18:03 -0400

    working on comment removal

--- main.js
+++ main.js
@@ -1,47 +1,74 @@
 document.getElementById('submit').addEventListener('click', (event) => {
   event.preventDefault();
 
+  //get the values of the entries for every post
   let postAuthor = document.getElementsByClassName('p-name')[0].value;
   let postContent = document.getElementsByClassName('post')[0].value;
 
+  //The content that will be display in each post, signed by whoever wrote it
   let post = `"${postContent}" Posted by ${postAuthor}`;
   let entry = document.createElement('p');
   entry.setAttribute("id", randomId());
   entry.innerHTML = post;
 
+  //Click to remove posts 
+  let removePost = document.createElement('button');
+  removePost.textContent = "Remove";
+  removePost.addEventListener('click', function() {
+    removePost(entry.id)
+  });
   
-  let removeButton = document.createElement('button');
-  removeButton.textContent = "Remove";
-  
-
-  
+//This decides where the post and removal button is going to display, as a child to each post 
   let createPost = document.getElementsByClassName('post-section')[0];
   createPost.appendChild(entry);
-  entry.appendChild(removeButton);
+  entry.appendChild(removePost);
 
-  document.getElementsByClassName('name')[0].value = '';
-  document.getElementsByClassName('comment')[0].value = '';
-  removeButton.addEventListener("click", function() {
+  //clear posts
+  document.getElementsByClassName('entry-form')[0].value = '';
+  
+  removePost.addEventListener("click", function() {
     removePost(this.parentNode.id)
 
   });
 });
 
 
+//comments
 
 
 document.getElementById('comment').addEventListener('click', (event) => {
   event.preventDefault(); 
-  let commentContent = document.getElementsByClassName('comment')[0].value = ''
+  
+  //get data from "Your reply" and "your comment"
+  let commentContent = document.getElementsByClassName('comment')[0].value;
   let commentAuthor = document.getElementsByClassName('c-name')[0].value;
+  
+  //What displays 
   let comment = `${commentAuthor} said "${commentContent}"`;
-
   let nextComment = document.createElement('p')
+  nextComment.setAttribute("id", randomId());
+  nextComment.innerHTML = comment;
 
+  //trigger when "reply to post" is clicked
   let createComment = document.getElementsByClassName('post-section')[0].appendChild(nextComment)
   nextComment.innerHTML = comment; 
 
-}); 
+
+  let removeComment = document.createElement('button');
+  removeComment.textContent = "Remove";
+  removeComment.addEventListener('click', function() {
+    document.getElementById(nextComment.id).remove();
+  });
+  //Position where the comment and remove botton will go
+  let commentArea = document.getElementsByClassName('comment-section')[0];
+  createPost.appendChild(nextComment);
+  nextComment.appendChild(removeComment);
+
+
+  document.getElementsByClassName('entry-form')[0].value = '';
+  
+
+});
 
 function randomId () {
  var id = Math.floor(Math.random()*100000000) 
